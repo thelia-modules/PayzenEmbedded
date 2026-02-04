3439652b378c4d2a3805a73efcf09b04298edb36
Thibault <thibs.n70@gmail.Com>
2026-02-04 11:19:16 +0100

    Fix typo in French invoice notification message

--- I18n/email/default/fr_FR.php
+++ I18n/email/default/fr_FR.php
@@ -7,6 +7,6 @@ return array(
     'The %store_name team' => 'L\'équipe %store_name',
     'The payment of your order is confirmed' => 'Le paiement de votre commande est confirmé.',
     'This is a confirmation of the payment of your order %ref via PayZen on our shop.' => 'Ceci est une confirmation du paiement de votre commande %ref avec PayZen',
-    'Your invoice is now available <a href="%account_url">in your customer account</a> on %store_name' => 'Votre facture est disponible <a href="%account_url">dans cotre compte client</a> %store_name',
+    'Your invoice is now available <a href="%account_url">in your customer account</a> on %store_name' => 'Votre facture est disponible <a href="%account_url">dans votre compte client</a> %store_name',
     'Your invoice is now available in your customer account at %link' => 'Votre facture est disponible dans votre compte client à l\'adresse %link',
 );
